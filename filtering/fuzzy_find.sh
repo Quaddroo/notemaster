858eff19444fd6f5b7548a0663a33df9c4939204
Kaspars Vandans <Kaspars.vandans@gmail.com>
2023-05-09 19:05:27 +0300

    fuzzy note finder

---
+++ filtering/fuzzy_find.sh
@@ -0,0 +1,3 @@
+# requires no_focus [title="forced_no_focus"] in i3wm to function
+
+OUTPUT=$(rg --heading --glob "*.q" . /home/qq/Notebooks/notemaster | sed 's/^$/NEWLINEHEREYO/g' | tr '\n' ' ' | sed 's/NEWLINEHEREYO/\n/g' | fzf --preview='FILEPATH=$(echo {} | sed "s/\.q.*/.q/"); cat $FILEPATH; FILEDIR=$(dirname $FILEPATH); IMAGENAME=$(grep -o -P -m 1 "(?<=\[img\[).*(?=\]\])" $FILEPATH); feh -B black -q --title "forced_no_focus" "$FILEDIR/$IMAGENAME" ') && FILEPATH=$(echo "$OUTPUT" | sed "s/\.q.*/.q/") && nvim $FILEPATH
