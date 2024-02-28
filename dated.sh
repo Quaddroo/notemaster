bcb75781467c7f3bf0e817dd8f76401d52614506
Kaspars Vandans <Kaspars.vandans@gmail.com>
2024-02-28 10:08:25 +0200

    date bugfix

--- dated.sh
+++ dated.sh
@@ -1,1 +1,1 @@
-echo "" | dmenu -p "date" -l 1 -nb '#3466a5' -nf yellow -sb '#3466a5' | xargs date --iso-8601 -d | xargs python3 templates/daily_note.py
+echo "" | dmenu -p "date" -l 1 -nb '#3466a5' -nf yellow -sb '#3466a5' | xargs -0 date --iso-8601 -d | xargs python3 templates/daily_note.py
